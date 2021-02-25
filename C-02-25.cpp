dcaa2116637f2d3e2382a7bc614372b04c658d7e
Wefun-cmd <77574156+Wefun-cmd@users.noreply.github.com>
2021-02-25 21:40:40 +0800

    Add files via upload

---
+++ C-02-25.cpp
@@ -0,0 +1,74 @@
+//#include<iostream>
+//#include<string>
+//#include<ctime>
+//#define day 7
+//using namespace std;
+//int main()
+//{
+//	/*cout << "hello world" << endl;*/
+//	/*int a = 10;
+//	cout << "a=" << a << endl;
+//	cout << "一周有" << day <<"天" << endl;*/
+//	/*cout << "" << sizeof(short) << endl;
+//	cout << "" << sizeof(int) << endl;
+//	cout << "" << sizeof(long) << endl;
+//	cout << "" << sizeof(long long) << endl;*/
+//	/*float f1 = 3.14;
+//	cout << "f1=" << f1 << endl;
+//	system("pause");*/
+//	//char ch = 'a';
+//	//cout << (int)ch << endl;
+//	/*string str = "abddoie";
+//	cout << str << endl;*/
+//	/*bool flag = true;
+//	cout << flag << endl;
+//	flag = false;
+//	cout << flag << endl;*/
+//	/*int a = 0;
+//	cout << "请输入一个数字:" << endl;
+//	cin >> a;
+//	cout << a << endl;*/
+//	/*int a = 10;
+//	int b = 20;
+//	int c = 0;
+//	c = (a > b ? a : b);
+//	cout << c << endl;*/
+//	/*srand((unsigned int)time(NULL));
+//	int num = rand() % 100 + 1;
+//	cout << num << endl;*/
+//	/*for (int i = 1; i < 10; i++)
+//	{
+//		for (int j = 1; j <= i; j++)
+//		{
+//			cout << j << "*" << i << "=" << i * j<<" ";
+//		}
+//		cout << endl;
+//	}*/
+//	int arr[10] = { 0,3,65,23,81,73,58,4,34,8 };
+//	int i = 0;
+//	for ( i = 0; i < 10-1; i++)
+//	{
+//		int j = 0;
+//		for ( j = 0; j < 10 -i- 1; j++)
+//		{
+//			if (arr[j] > arr[j + 1])
+//			{
+//				int tmp = arr[j];
+//				arr[j] = arr[j + 1];
+//				arr[j + 1] = tmp;
+//			}
+//		}
+//	}
+//	for ( int i = 0; i < 10; i++)
+//		cout << arr[i] << " ";
+//	system("pause");
+//	return 0;
+//}
+#include<iostream>
+using namespace std;
+int main()
+{
+
+	system("pause");
+	return 0;
+}
