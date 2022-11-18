88065add0e9b73150ff59dec20a690dd2e481a23
Wefun-cmd <77574156+Wefun-cmd@users.noreply.github.com>
2022-11-18 23:32:55 +0800

    Add files via upload

---
+++ 11-18.c
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+//int main()
+//{
+//	int arr1[5] = { 1,2,3,4,5 };
+//	int arr2[5] = { 2,3,4,5,6 };
+//	int arr3[5] = { 3,4,5,6,7 };
+//	int* parr[3] = { arr1, arr2, arr3 };
+//	int i = 0;
+//	for (i = 0; i < 3; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j < 5; j++)
+//		{
+//			printf("%d ", parr[i][j]);
+//		}
+//		printf("\n");
+//	}
+//}
+//数组指针：指向数组的指针。int* p1[10]:指针数组。int(*p2)[10]:数组指针
+int main()
+{
+	int a = 10;
+	int* p = &a;
+	int arr[10] = { 0 };
+	printf("%p\n", arr);
+	printf("%p\n", arr+1);
+	printf("%p\n", &arr[0]);
+	printf("%p\n", &arr[0]+1);
+	printf("%p\n", &arr);
+	printf("%p\n", &arr+1);
+	return 0;
+}
