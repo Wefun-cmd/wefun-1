2d829f7e8474db39d8a6f435bdb34b633845712e
Wefun-cmd <77574156+Wefun-cmd@users.noreply.github.com>
2022-12-01 23:06:56 +0800

    Add files via upload

---
+++ 12-1.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+#include <stdio.h>
+#include <string.h>
+
+//输入一个整数，将这个整数以字符串的形式逆序输出
+//int main() {
+//    int num = 0;
+//    while (~scanf("%d", &num))
+//    {
+//        if (num == 0)
+//        {
+//            printf("%d", num % 10);
+//        }
+//        while (num > 0)
+//        {
+//            printf("%d", num % 10);
+//            num /= 10;
+//        }
+//        printf("\n");
+//    }
+//    return 0;
+//}
