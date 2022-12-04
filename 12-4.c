ae40e0c5674d1c9819a6629f76ed8e2ae45d30de
Wefun-cmd <77574156+Wefun-cmd@users.noreply.github.com>
2022-12-04 22:56:34 +0800

    Add files via upload

---
+++ 12-4.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+#include <stdio.h>
+#include <string.h>
+
+//int main() {
+//    int n;
+//    while (~scanf("%d", &n))
+//    {
+//        int num1 = 1, num2 = 1, sum = 0;
+//        for (int i = 2; i < n; i++)
+//        {
+//            sum = num1 + num2;
+//            num1 = num2;
+//            num2 = sum;
+//        }
+//        printf("%d\n", sum);
+//    }
+//    return 0;
+//}
